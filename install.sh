5dc69e6df0be2a489d81c7c8eb556f317cad1534
JC Grubbs <jc.grubbs@devmynd.com>
2016-03-07 17:41:06 -0600

    initial commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+nativefier --name "Pipedrive" --icon ./pipedrive.png "http://devmynd.pipedrive.com" /Applications/
+nativefier --name "DevMynd Mail" --inject ./gmail.css --icon ./gmail.png "http://mail.devmynd.com" /Applications/
+nativefier --name "DevMynd Calendar" --icon ./calendar.png "http://calendar.devmynd.com" /Applications/
+nativefier --name "DevMynd Drive" --icon ./drive.png "http://drive.google.com" /Applications/
