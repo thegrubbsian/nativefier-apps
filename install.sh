c38afcf0b104b5e69c5897d70cf6125876fea13d
JC Grubbs <jc.grubbs@devmynd.com>
2016-05-13 18:12:19 -0500

    asana

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ nativefier --name "Pipedrive" --icon ./pipedrive.png "http://devmynd.pipedrive.c
 nativefier --name "DevMynd Mail" --inject ./gmail.css --icon ./gmail.png "http://mail.devmynd.com" /Applications/
 nativefier --name "DevMynd Calendar" --icon ./calendar.png "http://calendar.devmynd.com" /Applications/
 nativefier --name "DevMynd Drive" --icon ./drive.png "http://drive.google.com" /Applications/
+nativefier --name "Asana" --icon ./asana.png "https://app.asana.com" /Applications/
