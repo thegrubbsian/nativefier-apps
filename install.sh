fd18df5fa588ae5c7cb934d069d5cfa3d7bd2a54
JC Grubbs <jc.grubbs@devmynd.com>
2016-05-16 20:23:46 -0500

    disable context menu

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-nativefier --name "Pipedrive" --icon ./pipedrive.png "http://devmynd.pipedrive.com" /Applications/
-nativefier --name "DevMynd Mail" --inject ./gmail.css --icon ./gmail.png "http://mail.devmynd.com" /Applications/
-nativefier --name "DevMynd Calendar" --icon ./calendar.png "http://calendar.devmynd.com" /Applications/
-nativefier --name "DevMynd Drive" --icon ./drive.png "http://drive.google.com" /Applications/
-nativefier --name "Asana" --icon ./asana.png "https://app.asana.com" /Applications/
+nativefier --disable-context-menu --name "Pipedrive" --icon ./pipedrive.png "http://devmynd.pipedrive.com" /Applications/
+nativefier --disable-context-menu --name "DevMynd Mail" --inject ./gmail.css --icon ./gmail.png "http://mail.devmynd.com" /Applications/
+nativefier --disable-context-menu --name "DevMynd Calendar" --icon ./calendar.png "http://calendar.devmynd.com" /Applications/
+nativefier --disable-context-menu --name "DevMynd Drive" --icon ./drive.png "http://drive.google.com" /Applications/
+nativefier --disable-context-menu --name "Asana" --icon ./asana.png "https://app.asana.com" /Applications/
